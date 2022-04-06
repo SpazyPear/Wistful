4618430251e2890b1a00adabb0d04a5c8875ed0b
nin66 <nina.g162015@gmail.com>
2022-04-07 08:57:38 +1000

    Base system

---
+++ Assets/Scripts/Inventory/ItemObject.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemObject : MonoBehaviour
+{
+
+    public InventoryItemData referenceItem;
+
+
+    public void OnHandlePickupItem()
+    {
+       // InventorySystem.current.Add(referenceItem);
+        Destroy(gameObject);
+    }
+
+}
