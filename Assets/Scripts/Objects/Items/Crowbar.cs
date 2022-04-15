3886537a25f708c36691a68864af8304ec34be64
Chauchauz <chauchauzx@gmail.com>
2022-04-16 01:03:06 +1000

    Pseudocode

---
+++ Assets/Scripts/Objects/Items/Crowbar.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crowbar : MonoBehaviour
+{
+    Camera camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // When mouse clicks
+        // Raycast in front
+        // Check if glass
+        // Break glass
+    }
+}
