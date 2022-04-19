90b31b904cfd07a9e895f2966db5788c80ef82d3
myopia-droid <54422430+myopia-droid@users.noreply.github.com>
2022-04-19 17:18:10 +1000

    Login Page
    As a player, I want to be able to login to the computer in the office at the ‘real’ Mission Control using the password I found in the mirror Mission Control so I have to realise that the password found in the mirror world will be backwards.

--- Assets/Scripts/ArticleComputer.cs
+++ Assets/Scripts/ArticleComputer.cs
@@ -1,18 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
 
 public class ArticleComputer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public UIDocument loginPagePrefab;
+    private UIDocument currentLoginPage;
+    public UIDocument emailPrefab;
+    private TextField currentPasswordField; //Password field on login page
+    public bool loginTriggered = false; //Keeps track of whether the login page is open
+
+    private void OnTriggerStay(Collider other)
     {
-        
+        if (other.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.E) && !loginTriggered)
+            {
+                currentLoginPage = Instantiate(loginPagePrefab);
+                loginTriggered = true;
+                currentPasswordField = currentLoginPage.rootVisualElement.Q<TextField>();
+                currentPasswordField.Focus();
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(loginTriggered && Input.GetKeyDown(KeyCode.Escape)) //If on the login page and hit esc
+        {
+            //remove login page
+            loginTriggered = false;
+            if (currentLoginPage)
+            {
+                Destroy(currentLoginPage);
+            }
+        }
+
+        if (loginTriggered && Input.GetKeyDown(KeyCode.Return) || loginTriggered && Input.GetKeyDown(KeyCode.KeypadEnter)) //If on the login page and hit enter
+        {
+            
+
+            if (currentPasswordField.text.ToLower() == "m0ixvq8w" || currentPasswordField.text.ToLower() == "moixvq8w") //if correct
+            {
+                //remove login page
+                loginTriggered = false;
+                if (currentLoginPage)
+                {
+                    Destroy(currentLoginPage);
+                }
+
+                //get email page up
+                Instantiate(emailPrefab);
+            }
+        }
     }
 }
