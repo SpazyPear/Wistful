439ee9c93f6cd0a36e9252da20c4a7de6b5618d0
Jeffrey( Cheuk) Chung <13912201@student.uts.edu.au>
2022-04-19 22:21:27 +1000

    added UI text to give players instructions in the level

--- Assets/Scripts/PlayerCollisions.cs
+++ Assets/Scripts/PlayerCollisions.cs
@@ -14,6 +14,11 @@ public class PlayerCollisions : MonoBehaviour
     Door hitDoor;
     Item hitItem;
 
+    bool foundPhoto = false;
+    bool foundLadder = false;
+
+    bool foundRocket = false;
+    bool foundKite = false;
 
     private void Update()
     {
@@ -27,6 +32,9 @@ public class PlayerCollisions : MonoBehaviour
                 inventoryManager.pickUpItem(hitItem);
                 itemsHeld.Add(hitItem.itemID);
                 gameObject.AddComponent(hitItem.GetType());
+                CollectLevelOneItems();
+                uiManager.collectedObjectText.enabled = true;
+                 uiManager.collectedObjectText.text = "Collects " + hitItem.gameObject.name; 
                 (GetComponent(typeof(Item)) as Item).setItemProperties(hitItem.itemID, hitItem.prefab, hitItem.menuSprite, hitItem.description);
                 Destroy(hitItem.gameObject);
                 hitItem = null;
@@ -66,6 +74,35 @@ public class PlayerCollisions : MonoBehaviour
         if (collider.gameObject.GetComponent(typeof(Item)))
         {
             hitItem = null;
+        }
+    }
+
+    void CollectLevelOneItems()
+    {
+        switch(hitItem.gameObject.name)
+                {
+                    case "Stairs":
+                    uiManager.findObject2Text.enabled = false;
+                    foundLadder=true;
+                    break;
+                    case "Rocket":
+                    uiManager.findObject3Text.enabled = false;
+                    foundRocket=true;
+                    break;
+                    case "KitePrefab":
+                    uiManager.findObject4Text.enabled = false;
+                    foundKite = true;
+                    break;
+                    case "Object029": //should be photo
+                    uiManager.findObject1Text.enabled = false;
+                    foundPhoto = true;
+                    break;
+
+                }
+        if(foundKite && foundLadder && foundPhoto && foundRocket)
+        {
+             uiManager.collectedObjectText.text = "Go and find the Vault"; 
+              uiManager.collectedObjectText.fontSize = 19;
         }
     }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -9,11 +9,17 @@ public class UIManager : MonoBehaviour
     public Slider rocketFuelBar;
     public GameObject rocketFuelContainer;
     public Image rocketFuelBackground;
+    public Text findObject1Text;
+     public Text findObject2Text;
+      public Text findObject3Text;
+       public Text findObject4Text;
+
+        public Text collectedObjectText;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        collectedObjectText.enabled = false;
     }
 
     // Update is called once per frame
@@ -22,6 +28,7 @@ public class UIManager : MonoBehaviour
         
     }
 
+     
     public void UpdateRocketBar(float value)
     {
         rocketFuelBar.value = value;
