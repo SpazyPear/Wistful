a53d1d0799b4bfa9f11c248bd00b5a3ae3894360
Chauchauz <chauchauzx@gmail.com>
2022-04-20 01:21:03 +1000

    Glass Script

--- Assets/Scripts/Objects/Items/Crowbar.cs
+++ Assets/Scripts/Objects/Items/Crowbar.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Crowbar : MonoBehaviour
+public class Crowbar : Item
 {
     Camera camera;
+    float range = 2.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,8 +16,26 @@ public class Crowbar : MonoBehaviour
     void Update()
     {
         // When mouse clicks
+        if (Input.GetMouseButtonDown(0))
+        {
+            Hit();
+        }
         // Raycast in front
         // Check if glass
         // Break glass
+    }
+
+    void Hit()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, range))
+        {
+            Glass glass = hit.transform.GetComponent<Glass>();
+            if (glass != null)
+            {
+                glass.Shatter();
+            }
+        }
     }
 }
