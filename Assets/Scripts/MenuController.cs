d4f15a35ddbe95cf326da764519e05ba0462608b
Nuo <62932656+Nuo27@users.noreply.github.com>
2022-04-26 00:14:48 +0800

    Pause Menu function extended & bugs fixed

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -2,15 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class MenuController : MonoBehaviour
 {
     public bool GameisPause = false;
     public GameObject PauseMenu;
+    public Camera cam;
+
+    public GameObject Resume;
+    public GameObject Quit;
+    public GameObject Reset;
+
+    public GameObject Settings;
+    public AudioMixer audioMixer;
+    public bool SettingisActive;
+
+    public 
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameisPause = false;
     }
 
     // Update is called once per frame
@@ -23,7 +35,37 @@ public class MenuController : MonoBehaviour
         if (GameisPause)
         {
             Cursor.visible = true;
-            Cursor.lockState = CursorLockMode.None;        }
+            Cursor.lockState = CursorLockMode.None;        
+        }
+        //debug the current volumn  
+        Debug.Log(AudioListener.volume);
+
+    }
+    public void AwakeSettings(){
+        if (!SettingisActive)
+        {
+            Resume.SetActive(false);
+            Quit.SetActive(false);
+            Reset.SetActive(false);
+            Settings.SetActive(true);
+            SettingisActive = true;
+        }
+        else{
+            Resume.SetActive(true);
+            Quit.SetActive(true);
+            Reset.SetActive(true);
+            Settings.SetActive(false);
+            SettingisActive = false;
+        }
+        
+    }
+    public void setVolumn(float vol)
+    {
+        //AudioListener.volume = vol;
+        audioMixer.SetFloat("GameVolumn", vol);
+    }
+    public void applyFOV(float fovValue){
+        cam.fieldOfView = fovValue;
     }
     private void PausetheGame()
     {
@@ -38,6 +80,7 @@ public class MenuController : MonoBehaviour
         GameisPause = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
+
     public void ResettheLevel()
     {
         /*PauseMenu.SetActive(false);
@@ -45,6 +88,7 @@ public class MenuController : MonoBehaviour
         GameisPause = false;
         SceneManager.LoadScene("Level 1");*/
     }
+    
     public void ReturntoMain()
     {
         //
