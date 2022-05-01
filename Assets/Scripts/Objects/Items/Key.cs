d615ef5900167c3b384a1fc175d11baf16430812
myopia-droid <54422430+myopia-droid@users.noreply.github.com>
2022-05-01 14:29:17 +1000

    Mission Control Prefabs
    As a player, I want the mirror Mission Control to be split into three separate rooms so the environment feels uncanny.

--- Assets/Scripts/Objects/Items/Key.cs
+++ Assets/Scripts/Objects/Items/Key.cs
@@ -4,22 +4,9 @@ using UnityEngine;
 
 public class Key : Item
 {
-    public GameObject door;
-    //private bool doorLocked = true;
 
     private void Awake()
     {
         itemID = "Key";
-        //UseKey();
     }
-
-    /*public void UseKey()
-    {
-        if (doorLocked)
-        {
-            door.transform.Rotate(0, 90, 0);
-        }
-        doorLocked = false;
-    }*/
-
 }
