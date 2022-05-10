d0b912f504d09ae8f51a76c7b79fc6ba23ee3088
Jeffrey( Cheuk) Chung <13912201@student.uts.edu.au>
2022-05-11 00:30:34 +1000

    Added all my stuff to each scene

--- Assets/Scripts/FallingBlocks.cs
+++ Assets/Scripts/FallingBlocks.cs
@@ -31,12 +31,12 @@ public class FallingBlocks : MonoBehaviour
         if (!blockSpawned)
         {
             Instantiate(grassForFalling, spawnCoordinates, Quaternion.identity);
-            /*Instantiate(grassForFalling, new Vector3(spawnCoordinates.x + 8, spawnCoordinates.y, spawnCoordinates.z), Quaternion.identity);
+            Instantiate(grassForFalling, new Vector3(spawnCoordinates.x + 8, spawnCoordinates.y, spawnCoordinates.z), Quaternion.identity);
             Instantiate(grassForFalling, new Vector3(spawnCoordinates.x - 4, spawnCoordinates.y, spawnCoordinates.z), Quaternion.identity);
             Instantiate(grassForFalling, new Vector3(spawnCoordinates.x - 8, spawnCoordinates.y, spawnCoordinates.z), Quaternion.identity);
             Instantiate(grassForFalling, new Vector3(spawnCoordinates.x + 4, spawnCoordinates.y, spawnCoordinates.z), Quaternion.identity);
             Instantiate(grassForFalling, new Vector3(spawnCoordinates.x, spawnCoordinates.y, spawnCoordinates.z - 4), Quaternion.identity);
-            Instantiate(grassForFalling, new Vector3(spawnCoordinates.x, spawnCoordinates.y, spawnCoordinates.z + 4), Quaternion.identity);*/
+            Instantiate(grassForFalling, new Vector3(spawnCoordinates.x, spawnCoordinates.y, spawnCoordinates.z + 4), Quaternion.identity);
             grassForFalling.GetComponent<Rigidbody>().AddForce(new Vector3(0, -5000.0f * spawnRate, 0), ForceMode.Impulse);
             grassForFalling.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
             blockSpawned = true;
