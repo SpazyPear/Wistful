01ec46040b8ab98af75c8f75eaf18ebf7b515424
nin66 <nina.g162015@gmail.com>
2022-05-27 21:02:09 +1000

    cutscene2

---
+++ Assets/NarrationWindow.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+
+public class NarrationWindow : MonoBehaviour
+{
+
+    public GameObject UiObject;
+    public GameObject theItem;
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        UiObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            UiObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        StartCoroutine(WaitBeforeDisappear());
+        
+    }
+
+    IEnumerator WaitBeforeDisappear()
+    {
+        yield return new WaitForSeconds(5);
+        UiObject.SetActive(false);
+        Destroy(theItem);
+    }
+}
--- Assets/Scenes/Cutscenes/TriggerCutscene.cs
+++ Assets/Scenes/Cutscenes/TriggerCutscene.cs
@@ -7,23 +7,28 @@ public class TriggerCutscene : MonoBehaviour
 
     [SerializeField] GameObject _cutscene;
     //public GameObject thePlayer;
+    public GameObject PlayerPrefab;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("TIME"))
         {
             _cutscene.SetActive(true);
-            //thePlayer.SetActive(false);
+            PlayerPrefab.gameObject.GetComponent<Movement>().enabled = false;
             StartCoroutine(FinishCut());
+            StartCoroutine(MovementEnable());
         }
     }
 
     IEnumerator FinishCut()
     {
-        yield return new WaitForSeconds(5);
+        yield return new WaitForSeconds(1);
         GameObject.FindGameObjectWithTag("TIME").GetComponent<BoxCollider>().enabled = false;
-        //thePlayer.SetActive(true);
+    }
 
-
+    IEnumerator MovementEnable()
+    {
+        yield return new WaitForSeconds(39);
+        PlayerPrefab.gameObject.GetComponent<Movement>().enabled = true;
     }
 }
